c91f3c3879e268580b4dad39e93d654765752496
nasrinio <86842564+nasrinio@users.noreply.github.com>
2023-09-07 03:57:45 +0300

    user and post relation using mongo

---
+++ user & posts mongo/src/userControler/router.js
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import * as user from "./controller/user.js";
+
+const router = Router();
+router.get('/', (req, res) => {
+    res.json({ message: "user API" })
+})
+
+
+
+
+router.post('/signup', user.signUp)
+
+router.post('/signin', user.signIn)
+
+router.patch('/update/:_id', user.updateUser)
+
+router.delete('/delete/:_id', user.deleteUser)
+
+router.get('/age-between/:maxAge/:minAge', user.ageBetween)
+
+router.get('/getAllUsers', user.getAllUsers)
+
+router.get('/get-profile/:id', user.getUserWithNotes)
+
+router.get('/searchUser', user.searchUser)
+
+
+
+
+
+export default router
